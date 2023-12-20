d9a8b5d123f726ec74e834087e3f303d33308ee0
Michael Ponti <michael.ponti@live.ca>
2023-12-20 14:22:14 -0800

    update for static web app

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # resume
 
 Setup my resume.
+
+## DocFx
+
+Currently using docfx to generate a static html site from markdown files. Version 2.59.2.0.
+
+## Deployment Process
+
+Site is deployed to an Azure Static Web App. It is managed using the Azure SWA CLI which is documented [here](https://azure.github.io/static-web-apps-cli/docs/use/install).
--- build-static-site.ps1
+++ build-static-site.ps1
@@ -1,12 +1,4 @@
 
 docfx build .\docfx_project\docfx.json
-Remove-Item .\publish.zip
 
-# for the credentials, we need to go deployment center and to the ftps credentials tab.
-# in user scope, user name should be <appname>\user, password should be whatever is
-# required 
-
-
-Compress-Archive -Path .\docfx_project\_site\* -DestinationPath .\publish.zip -Force
-$AuthHeaderRaw = $Env:michaelpontideployid + ":" + $Env:michaelpontideploypw
-curl -X POST -H "Content-Type: application/zip" -u $AuthHeaderRaw  --data-binary '@./publish.zip' --verbose https://michaelponti.scm.azurewebsites.net/api/zipdeploy
+swa deploy -d $Env:michaelpontideploytoken -a .\docfx_project\_site --env production
